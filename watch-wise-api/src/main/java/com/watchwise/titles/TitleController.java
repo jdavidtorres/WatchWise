531cfa12384bad256813a49dc8fec964b6bc69a7
Yisus <78105017+jdavidtorres@users.noreply.github.com>
2025-08-29 23:48:14 -0500

    feat(main): add title search endpoint

---
+++ watch-wise-api/src/main/java/com/watchwise/titles/TitleController.java
@@ -0,0 +1,27 @@
+package com.watchwise.titles;
+
+import com.watchwise.trakt.TraktApiClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Titles")
+@RestController
+@RequestMapping("/titles")
+public class TitleController {
+
+    private final TraktApiClient traktApiClient;
+
+    public TitleController(TraktApiClient traktApiClient) {
+        this.traktApiClient = traktApiClient;
+    }
+
+    @Operation(summary = "Search titles by query")
+    @GetMapping
+    public String search(@RequestParam String query) {
+        return traktApiClient.searchMovie(query);
+    }
+}
