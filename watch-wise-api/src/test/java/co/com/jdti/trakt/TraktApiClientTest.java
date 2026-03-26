2abef74ccb03050d114371c5d0a1863fd5ae3e68
Yisus <jesu_ti91@hotmail.com>
2026-03-26 08:25:27 -0500

    Use explicit MockRestRequestMatchers and Assertions
    Replace static test imports with explicit MockRestRequestMatchers and MockRestResponseCreators and switch to Assertions.assertThat. Make MockRestServiceServer creation null-safe by falling back to a new RestTemplate, and tidy up field references/formatting in TraktApiClientTest to improve clarity.

--- watch-wise-api/src/test/java/co/com/jdti/trakt/TraktApiClientTest.java
+++ watch-wise-api/src/test/java/co/com/jdti/trakt/TraktApiClientTest.java
@@ -1,39 +1,38 @@
 package co.com.jdti.trakt;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.web.client.RestTemplate;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 class TraktApiClientTest {
 
-    private MockRestServiceServer server;
-    private TraktApiClient client;
+	private MockRestServiceServer server;
+	private TraktApiClient client;
 
-    @BeforeEach
-    void setUp() {
-        TraktApiProperties props = new TraktApiProperties("http://localhost", "test-key");
-        RestTemplate restTemplate = new RestTemplateBuilder().rootUri(props.baseUrl()).build();
-        server = MockRestServiceServer.createServer(restTemplate);
-        client = new TraktApiClient(restTemplate, props);
-    }
+	@BeforeEach
+	void setUp() {
+		TraktApiProperties props = new TraktApiProperties("http://localhost", "test-key");
+		RestTemplate restTemplate = new RestTemplateBuilder().rootUri(props.baseUrl()).build();
+		this.server = MockRestServiceServer.createServer(restTemplate != null ? restTemplate : new RestTemplate());
+		this.client = new TraktApiClient(restTemplate, props);
+	}
 
-    @Test
-    void searchMovieSendsRequiredHeaders() {
-        server.expect(requestTo("http://localhost/search/movie?query=inception"))
-            .andExpect(method(HttpMethod.GET))
-            .andExpect(header("trakt-api-key", "test-key"))
-            .andExpect(header("trakt-api-version", "2"))
-            .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
+	@Test
+	void searchMovieSendsRequiredHeaders() {
+		this.server.expect(MockRestRequestMatchers.requestTo("http://localhost/search/movie?query=inception"))
+				.andExpect(MockRestRequestMatchers.method(HttpMethod.GET))
+				.andExpect(MockRestRequestMatchers.header("trakt-api-key", "test-key"))
+				.andExpect(MockRestRequestMatchers.header("trakt-api-version", "2"))
+				.andRespond(MockRestResponseCreators.withSuccess("[]", MediaType.APPLICATION_JSON));
 
-        String response = client.searchMovie("inception");
-        assertThat(response).isEqualTo("[]");
-    }
+		String response = this.client.searchMovie("inception");
+		Assertions.assertThat(response).isEqualTo("[]");
+	}
 }
